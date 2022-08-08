2a68c8c9e9a3426c2d20d3d88f6b2be66f7f04ea
Станислав Шубин <sti.99@mail.ru>
2022-08-08 16:20:02 +0700

    better

--- widget/widget.cpp
+++ widget/widget.cpp
@@ -3,40 +3,7 @@
 
 #include "visual.h"
 #include <math.h>
-
-template<size_t s>
-class graphics
-{
-    float points[s]={0};
-    float time;
-    size_t cur=0;
-    float wait;
-public:
-    static void timer(void*d)
-    {
-        ((visual<1,2>*)d)->redraw();
-        Fl::repeat_timeout(.025,timer,d);
-    }
-
-    inline
-    void start(const float w,void*d)
-    {wait=w;Fl::add_timeout(.025,timer,d);}
-
-    inline
-    void next(const float t)
-    {points[cur]=t;cur=(cur+1)%s;}
-
-    void paint()
-    {
-        glBegin(GL_LINE_STRIP);
-        
-         const size_t temp=s-1;
-        for(size_t i=1;i!=temp;i++)
-            glVertex2f(float(i)/s,points[(i+cur)%temp]);
-
-        glEnd();
-    }
-};
+#include "graphics.h"
 
 int main()
 {
@@ -51,7 +18,7 @@ int main()
             temp->paint();
         }
         ,&g,x);
-    g.start(.025,&graph);
+    g.start(.0025,&graph);
     
     win.end();
     win.show();
